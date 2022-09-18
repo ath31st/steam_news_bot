2bed981aeaa444a6086553cee37ff7f4134e32c3
ath31st <ath31st@yandex.ru>
2022-09-18 11:44:28 +0300

    added UserServiceTest

---
+++ src/test/java/bot/farm/steam_news_bot/service/UserServiceTest.java
@@ -0,0 +1,128 @@
+package bot.farm.steam_news_bot.service;
+
+import bot.farm.steam_news_bot.entity.User;
+import bot.farm.steam_news_bot.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private UserGameStateService userGameStateService;
+    @Mock
+    private SteamService steamService;
+    @InjectMocks
+    private UserService userService;
+
+    private String chatId;
+    private String name;
+    private String streamId;
+    private String locale;
+    private boolean active;
+
+    @BeforeEach
+    void setUp() {
+        chatId = "1";
+        name = "Test";
+        streamId = "76561198131767661";
+        locale = "ru";
+        active = true;
+    }
+
+    @Test
+    void saveUser() throws IOException {
+        doReturn(Optional.empty()).when(userRepository).findUserByChatId(chatId);
+        userService.saveUser(chatId, name, streamId, locale);
+        verify(userRepository, times(1)).findUserByChatId(chatId);
+    }
+
+    @Test
+    void updateUser() throws IOException {
+        userService.updateUser(chatId, streamId, locale);
+        verify(userRepository, times(1)).findUserByChatId(chatId);
+    }
+
+    @Test
+    void getCountOwnedGames() {
+        doReturn(100L).when(userRepository).countByChatIdAndStates_IsOwnedTrue(chatId);
+        assertEquals(100L, userService.getCountOwnedGames(chatId));
+    }
+
+    @Test
+    void updateActiveForUser() {
+        doReturn(Optional.of(new User())).when(userRepository).findUserByChatId(chatId);
+        userService.updateActiveForUser(chatId, active);
+        verify(userRepository, times(1)).findUserByChatId(chatId);
+        verify(userRepository, times(1)).updateActiveByChatId(active, chatId);
+    }
+
+    @Test
+    void findUserByChatId() {
+        userService.findUserByChatId(chatId);
+        verify(userRepository, times(1)).findUserByChatId(chatId);
+    }
+
+    @Test
+    void getAllUsers() {
+        assertEquals(0, userService.getAllUsers().size());
+        verify(userRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getUsersWithFilters() {
+        String appid = "440";
+        assertEquals(0, userService.getUsersWithFilters(appid).size());
+        verify(userRepository, times(1)).findByActiveTrueAndStates_Game_AppidAndStates_IsBannedFalse(appid);
+    }
+
+    @Test
+    void getUsersByActive() {
+        assertEquals(0, userService.getUsersByActive(active).size());
+        verify(userRepository, times(1)).findByActive(active);
+    }
+
+    @Test
+    void countUsersByActive() {
+        doReturn(10L).when(userRepository).countByActive(active);
+        assertEquals(10L, userService.countUsersByActive(active));
+    }
+
+    @Test
+    void existsByChatId() {
+        assertFalse(userService.existsByChatId(chatId));
+    }
+
+    @Test
+    void getUserByChatId() {
+        User user = new User();
+        user.setLocale(locale);
+        user.setChatId(chatId);
+        user.setActive(active);
+        user.setName(name);
+        user.setSteamId(Long.valueOf(streamId));
+
+        doReturn(user).when(userRepository).findByChatId(chatId);
+        userService.getUserByChatId(chatId);
+        verify(userRepository, times(1)).findByChatId(chatId);
+    }
+
+    @Test
+    void checkBanForGameByChatId() {
+        assertFalse(userService.checkBanForGameByChatId(chatId, name));
+        verify(userRepository, times(1)).existsByChatIdAndStates_Game_NameAndStates_IsBannedTrue(chatId, name);
+    }
+
+}
