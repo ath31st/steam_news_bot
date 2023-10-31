013c2cb66b2db31ff79494ede993ba16178612ef
ath31st <ath31st@yandex.ru>
2023-10-31 12:40:04 +0300

    extended test for AnnouncementService

--- src/test/java/bot/farm/steam_news_bot/service/AnnouncementServiceTest.java
+++ src/test/java/bot/farm/steam_news_bot/service/AnnouncementServiceTest.java
@@ -2,9 +2,12 @@ package bot.farm.steam_news_bot.service;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import bot.farm.steam_news_bot.SteamNewsBot;
 import bot.farm.steam_news_bot.dto.Message;
+import bot.farm.steam_news_bot.entity.User;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,9 +25,13 @@ class AnnouncementServiceTest {
 
   @Test
   void receiveMessageAndNotificationUsers() {
+    User user = new User();
+    user.setChatId("1");
     Message message = new Message();
     message.setText("testing test");
     message.setAuthor("Test");
+
+    when(userService.getUsersByActive(true)).thenReturn(List.of(user));
 
     announcementService.receiveMessageAndNotificationUsers(message);
     verify(userService, times(1)).getUsersByActive(true);
