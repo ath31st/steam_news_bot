7bc12fe1b4562e643c2b7387cbdb27b4d048da94
ath31st <ath31st@yandex.ru>
2025-02-21 10:25:55 +0300

    add GameRepository

---
+++ steam_news_bot_kt/src/main/kotlin/repository/GameRepository.kt
@@ -0,0 +1,54 @@
+package sidim.doma.repository
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import sidim.doma.entity.Game
+import sidim.doma.entity.Games
+import sidim.doma.entity.UserGameStates
+import sidim.doma.entity.Users
+
+class GameRepository {
+    fun findBannedByChatId(chatId: String): List<Game> {
+        return transaction {
+            (Games innerJoin UserGameStates)
+                .selectAll()
+                .where {
+                    (UserGameStates.userId eq chatId) and
+                            (UserGameStates.isBanned eq true)
+                }
+                .map { rowToGame(it) }
+        }
+    }
+
+    fun findByActiveUsersAndNotBanned(): List<Game> {
+        return transaction {
+            (Games innerJoin UserGameStates innerJoin Users)
+                .selectAll()
+                .where {
+                    (Users.active eq true) and
+                            (UserGameStates.isBanned eq false)
+                }
+                .distinct()
+                .map { rowToGame(it) }
+        }
+    }
+
+    fun findTopGames(limit: Long): List<Game> {
+        return transaction {
+            (Games innerJoin UserGameStates)
+                .selectAll()
+                .where {
+                    (UserGameStates.isBanned eq false)
+                }
+                .groupBy(Games.name)
+                .having { UserGameStates.gameId.count() greater limit }
+                .orderBy(UserGameStates.gameId.count() to SortOrder.DESC)
+                .map { rowToGame(it) }
+        }
+    }
+
+    private fun rowToGame(it: ResultRow) = Game(
+        appid = it[Games.appid],
+        name = it[Games.name]
+    )
+}
