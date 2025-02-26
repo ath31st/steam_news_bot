75f407451793753f95f0e0bed82dfa0a71ea34cd
ath31st <ath31st@yandex.ru>
2025-02-26 11:37:05 +0300

    update Game entity and object with nullable name (game name)

--- steam_news_bot_kt/src/main/kotlin/entity/Games.kt
+++ steam_news_bot_kt/src/main/kotlin/entity/Games.kt
@@ -6,12 +6,12 @@ import org.jetbrains.exposed.sql.Table
 
 data class Game @JsonCreator constructor(
     @JsonProperty("appid") val appid: String,
-    @JsonProperty("name") val name: String
+    @JsonProperty("name") val name: String?
 )
 
 object Games : Table("Games") {
     val appid = varchar("game_id", 255)
-    val name = varchar("name", 255)
+    val name = varchar("name", 255).nullable()
 
     override val primaryKey = PrimaryKey(appid)
 }
