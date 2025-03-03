15cb6bec4eaf23a11ae13a267b7b0918a1ae7460
ath31st <ath31st@yandex.ru>
2025-03-03 11:45:11 +0300

    update NewsItem entity for working with jackson

--- steam_news_bot_kt/src/main/kotlin/entity/NewsItems.kt
+++ steam_news_bot_kt/src/main/kotlin/entity/NewsItems.kt
@@ -1,19 +1,21 @@
 package sidim.doma.entity
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.jetbrains.exposed.sql.Table
 
-data class NewsItem(
-    val gid: Long,
-    val title: String,
-    val url: String,
-    val author: String,
-    var contents: String,
-    val isExternalUrl: Boolean,
-    val feedLabel: String,
-    val feedName: String,
-    val feedType: Int,
-    val appid: String,
-    val date: String
+data class NewsItem @JsonCreator constructor(
+    @JsonProperty("gid") val gid: Long,
+    @JsonProperty("title") val title: String,
+    @JsonProperty("url") val url: String,
+    @JsonProperty("author") val author: String,
+    @JsonProperty("contents") var contents: String,
+    @JsonProperty("is_external_url") val isExternalUrl: Boolean,
+    @JsonProperty("feedlabel") val feedLabel: String,
+    @JsonProperty("feedname") val feedName: String,
+    @JsonProperty("feed_type") val feedType: Int,
+    @JsonProperty("appid") val appid: String,
+    @JsonProperty("date") val date: String
 )
 
 object NewsItems : Table("news_item") {
