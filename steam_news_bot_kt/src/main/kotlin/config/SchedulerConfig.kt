42c20c646727dcfce154b51a2aed3fbdd514aa5a
ath31st <ath31st@yandex.ru>
2025-03-13 10:15:48 +0300

    add SchedulerConfig with constants

---
+++ steam_news_bot_kt/src/main/kotlin/config/SchedulerConfig.kt
@@ -0,0 +1,10 @@
+package sidim.doma.config
+
+object SchedulerConfig {
+    const val SEMAPHORE_LIMIT = 100
+    const val CHUNK_SIZE = 10
+    const val PROBLEM_GAMES_ATTEMPTS = 3
+    const val PROBLEM_GAMES_DELAY = 5
+    const val NEWS_ITEMS_DELAY = 30
+    const val GAME_STATES_DELAY = 24
+}
