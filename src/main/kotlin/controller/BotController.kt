45614d349cefe1c87c16fcdfc2ceb0a057f71756
ath31st <ath31st@yandex.ru>
2025-04-17 12:01:01 +0300

    rework BotController with command handler

--- src/main/kotlin/controller/BotController.kt
+++ src/main/kotlin/controller/BotController.kt
@@ -2,107 +2,36 @@ package sidim.doma.controller
 
 import dev.inmo.tgbotapi.extensions.api.answers.answerCallbackQuery
 import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
-import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onDataCallbackQuery
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onText
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.from
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.text
-import dev.inmo.tgbotapi.types.chat.CommonUser
-import dev.inmo.tgbotapi.types.message.abstracts.CommonMessage
 import dev.inmo.tgbotapi.utils.RiskFeature
-import sidim.doma.service.UserInteractionService
-import sidim.doma.util.Localization.getText
+import org.slf4j.LoggerFactory
+import sidim.doma.service.CallbackCommandRegistry
+import sidim.doma.service.CommandHandler
+import sidim.doma.service.UserInteraction
+import sidim.doma.util.LocalizationUtils.getText
+import sidim.doma.util.LocalizationUtils.getUserLocale
 
 class BotController(
-    private val interactionService: UserInteractionService
+    private val interaction: UserInteraction
 ) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+    private val commandHandler = CommandHandler(interaction)
+    private val callbackCommandRegistry = CallbackCommandRegistry(interaction)
+
     @OptIn(RiskFeature::class)
     suspend fun registerHandlers(context: BehaviourContext) {
+        logger.info("Registering bot handlers")
+        commandHandler.registerCommands(context)
+
         with(context) {
-            onCommand("start") { message ->
-                val chatId = message.chat.id
-                val locale = getUserLocale(message)
-                interactionService.handleStart(chatId, locale)
-            }
-
-            onCommand("help") { message ->
-                val chatId = message.chat.id
-                val locale = getUserLocale(message)
-                interactionService.handleHelp(chatId, locale)
-            }
-
-            onCommand("settings") { message ->
-                val chatId = message.chat.id
-                val locale = getUserLocale(message)
-                interactionService.handleSettings(chatId, locale)
-            }
-
-            onCommand("stats") { message ->
-                val chatId = message.chat.id
-                val locale = getUserLocale(message)
-                interactionService.handleStats(chatId, locale)
-            }
-
             onDataCallbackQuery { callback ->
                 val chatId = callback.from.id
                 val locale = callback.user.languageCode ?: "en"
-                when {
-                    callback.data == "/set_steam_id" -> interactionService.handleSetSteamId(
-                        chatId,
-                        locale
-                    )
-
-                    callback.data == "/check_steam_id" -> interactionService.handleCheckSteamId(
-                        chatId,
-                        locale
-                    )
-
-                    callback.data == "/set_active_mode" -> interactionService.handleSetActiveMode(
-                        chatId,
-                        locale
-                    )
-
-                    callback.data == "/set_inactive_mode" -> interactionService.handleSetInactiveMode(
-                        chatId,
-                        locale
-                    )
-
-                    callback.data == "/check_wishlist" -> interactionService.handleCheckWishlist(
-                        chatId,
-                        locale
-                    )
-
-                    callback.data.startsWith("/subscribe_") -> interactionService.handleSubscribe(
-                        chatId,
-                        callback.data,
-                        locale
-                    )
-
-                    callback.data.startsWith("/unsubscribe") -> interactionService.handleUnsubscribe(
-                        chatId,
-                        callback.data,
-                        locale
-                    )
-
-                    callback.data.startsWith("/links_to_game") -> interactionService.handleLinksToGame(
-                        chatId,
-                        callback.data,
-                        locale
-                    )
-
-                    callback.data.startsWith("/black_list") -> interactionService.handleBlackList(
-                        chatId,
-                        callback.data,
-                        locale,
-                    )
-
-                    callback.data == "/clear_black_list" -> interactionService.handleClearBlackList(
-                        chatId,
-                        locale
-                    )
-
-                    else -> interactionService.handleUnknownCommand(chatId, locale)
-                }
+                logger.info("Handling callback query: ${callback.data} for chatId: $chatId")
+                callbackCommandRegistry.executeCommand(chatId, callback.data, locale)
                 bot.answerCallbackQuery(callback)
             }
 
@@ -110,13 +39,9 @@ class BotController(
                 val chatId = message.chat.id
                 val locale = getUserLocale(message)
                 val name = message.from?.username?.username ?: getText("users.default_name", locale)
-
-                interactionService.handleTextInput(chatId, name, message.text ?: "", locale)
+                logger.info("Handling text input for chatId: $chatId, text: ${message.text}")
+                interaction.handleTextInput(chatId, name, message.text ?: "", locale)
             }
         }
     }
-
-    @OptIn(RiskFeature::class)
-    private fun getUserLocale(message: CommonMessage<*>): String =
-        (message.from as CommonUser).languageCode ?: "en"
 }
