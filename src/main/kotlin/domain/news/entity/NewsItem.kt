7edf186cb9ad0081b010a337fbd1b512d17a74f5
ath31st <ath31st@yandex.ru>
2025-04-18 12:55:41 +0300

    change NewsItem fields

--- src/main/kotlin/domain/news/entity/NewsItem.kt
+++ src/main/kotlin/domain/news/entity/NewsItem.kt
@@ -1,21 +1,15 @@
 package sidim.doma.domain.news.entity
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import org.jetbrains.exposed.sql.Table
 
-data class NewsItem @JsonCreator constructor(
-    @JsonProperty("gid") val gid: Long,
-    @JsonProperty("title") val title: String,
-    @JsonProperty("url") val url: String,
-    @JsonProperty("author") val author: String,
-    @JsonProperty("contents") val contents: String,
-    @JsonProperty("is_external_url") val isExternalUrl: Boolean,
-    @JsonProperty("feedlabel") val feedLabel: String,
-    @JsonProperty("feedname") val feedName: String,
-    @JsonProperty("feed_type") val feedType: Int,
-    @JsonProperty("appid") val appid: String,
-    @JsonProperty("date") val date: Long
+data class NewsItem(
+    val gid: Long,
+    val title: String,
+    val url: String,
+    val author: String,
+    val contents: String,
+    val appid: String,
+    val date: Long
 )
 
 object NewsItems : Table("news_item") {
@@ -24,10 +18,6 @@ object NewsItems : Table("news_item") {
     val url = varchar("url", 255)
     val author = varchar("author", 255)
     val contents = varchar("contents", 255)
-    val isExternalUrl = bool("is_external_url")
-    val feedLabel = varchar("feed_label", 255)
-    val feedName = varchar("feed_name", 255)
-    val feedType = integer("feed_type")
     val appid = varchar("appid", 255)
     val date = long("date")
 
