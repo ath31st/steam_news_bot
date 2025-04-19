f104ea5497248aa12a325b376e16f4ae6cd78f84
ath31st <ath31st@yandex.ru>
2025-04-19 10:59:37 +0300

    delete extra code and constants

--- src/main/kotlin/application/game/mapper/GameMapper.kt
+++ src/main/kotlin/application/game/mapper/GameMapper.kt
@@ -1,14 +1,8 @@
 package sidim.doma.application.game.mapper
 
 import sidim.doma.domain.game.entity.Game
-import sidim.doma.infrastructure.integration.steam.dto.SteamAppDetailsDto
 import sidim.doma.infrastructure.integration.steam.dto.SteamAppDto
 import sidim.doma.infrastructure.integration.steam.dto.SteamWishlistAppDto
-
-fun SteamAppDetailsDto.toGame() = Game(
-    appid = appid,
-    name = name
-)
 
 fun SteamAppDto.toGame() = Game(
     appid = appid.toString(),
--- src/main/kotlin/application/scheduler/config/SchedulerConfig.kt
+++ src/main/kotlin/application/scheduler/config/SchedulerConfig.kt
@@ -17,8 +17,6 @@ object SchedulerConfig {
     const val GAME_STATES_START_JOB_DELAY = 60L // seconds
 
     const val UPDATE_GAMES_JOB_DELAY = 2 // hours
-    const val UPDATE_GAMES_START_JOB_DELAY = 2L // seconds
+    const val UPDATE_GAMES_START_JOB_DELAY = 240L // seconds
     const val UPDATE_GAMES_CHUNK_SIZE = 40
-    const val UPDATE_GAMES_JOB_LIMIT = 200 // games
-    const val UPDATE_GAMES_DELAY = 1000L // 1 second
 }
